160ab45185068f2b330fde8804deb48ef44943e8
arungum <arungum@gmail.com>
2021-08-10 23:00:58 +0530

    my second push

---
+++ src/org/tester/Compan.java
@@ -0,0 +1,14 @@
+package org.tester;
+
+public class Compan {
+	public void a1() {
+		System.out.println("a1");
+	}
+	public void a2() {
+		System.out.println("a2");
+	}public void a3() {
+		System.out.println("a3");
+	}public void a4() {
+		System.out.println("a4");
+	}
+}
